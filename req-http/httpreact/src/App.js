99d6db1c987a7ea312be5403b8469af42ae1ecfd
jeanlucasbs <jean.silva@discente.ufg.br>
2022-09-12 10:41:13 -0300

    initial commit

---
+++ req-http/httpreact/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import {useState, useEffect} from 'react';
+
+const url = 'http://localhost:3000/products';
+
+function App() {
+  const [products, setProducts] = useState([]);
+  
+  //1- resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+    
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Lista de Produtos</h1>
+      {products.map((product) => (
+        <li key={product.id}>{product.name}- R${product.price}</li>
+      ))}
+    </div>
+  );
+}
+
+export default App;
