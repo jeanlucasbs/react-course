7a5b58a4e963077a18285cd6061e3e3efb5f8078
jeanlucasbs <jean.silva@discente.ufg.br>
2022-09-21 12:33:14 -0300

    updates de estudo

--- react-router/reactrouter/src/App.js
+++ react-router/reactrouter/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 //1- Config React Router
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 
 //pages
 import Home from './pages/Home';
@@ -9,9 +9,11 @@ import About from './pages/About';
 import Product from './pages/Product';
 import Info from './pages/Info';
 import Page404 from './pages/Page404';
+import Search from './pages/Search';
 
 //navegação
 import Navbar from './components/Navbar';
+import SearchForm from './components/SearchForm';
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
       <h1>React Router</h1>
       <BrowserRouter>
         <Navbar/>
+        {/*Search */}
+        <SearchForm/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
@@ -31,7 +35,12 @@ function App() {
 
           {/*no match route */}
           <Route path="*" element={<Page404/>}/>
-          
+
+          {/*Search */}
+          <Route path="/search" element={<Search/>}/>
+
+          {/*Redirect */}
+          <Route path="/company" element={<Navigate to="/about"/>}/>
         </Routes>
       </BrowserRouter>
     </div>
